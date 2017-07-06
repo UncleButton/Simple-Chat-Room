536bc85d065ea5411e067fb1809e270e3fd66f95
Britton Bailer <britton.bailer@gmail.com>
2017-07-06 13:14:33 -0400

    making it my own hehehe

--- public/script.js
+++ public/script.js
@@ -22,14 +22,17 @@ var unescapeHTML = function(str) {
 
 var htmlEscaper = /[&<>"'\/]/g;
 
-var pseudo = escape(prompt('Choose a pseudo')) || 'Anonymous';
+var pseudo = escape(prompt('Choose a nickname for yourself.')) || 'Anonymous';
 
 var socket = io.connect();
 
+var d = new Date();
+
 socket.emit('new-user', unescapeHTML(pseudo));
 
-socket.on('new-user', function(pseudo) {
-	$('#conversation').prepend('<div class="animated fadeInLeft"><p><em><strong>' + pseudo + '</strong>  has joined the chat.</em><p><hr></div>');
+socket.on('new-user', function(pseudo) {//when a person joins, do this
+	d = new Date();
+	$('#conversation').prepend('<div class="animated fadeInLeft"><p class="new_user"><em><strong>' + pseudo + '</strong>  has joined the chat.</em> <div class="timeStamp"> <em>'+(d.getHours()>12?d.getHours()-12:d.getHours())+":"+(d.getMinutes()<10?'0':'')+d.getMinutes()+" "+(d.getHours()>12?"PM":"AM")+'</em></div></p><hr></div>');
 	//Add animation
 	$('#conversation').addClass('animated fadeInDown');
 	//and then remove it
@@ -38,8 +41,9 @@ socket.on('new-user', function(pseudo) {
 	});
 });
 
-socket.on('message', function(data) {
-	$('#conversation').prepend('<div class="animated fadeInLeft"><p class="name he">' + data.pseudo + '</p><p class="message he">' + data.message + '</p><hr></div>');
+socket.on('message', function(data) {//when someone sends a message, do this
+	d = new Date();
+	$('#conversation').prepend('<div class="animated fadeInLeft"><p class="name he">' + data.pseudo + '</p><p class="message he">' + data.message + '<div class="timeStamp"> <em>'+(d.getHours()>12?d.getHours()-12:d.getHours())+":"+(d.getMinutes()<10?'0':'')+d.getMinutes()+" "+(d.getHours()>12?"PM":"AM")+'</em></div></p><hr></div>');
 	//Add animation
 	$('#conversation').addClass('animated fadeInDown');
 	//and then remove it when animation ends
@@ -49,11 +53,12 @@ socket.on('message', function(data) {
 });
 
 var input = $('input');
-input.keyup(function(event) {
+input.keyup(function(event) {//when you send a message, do this
 	if (event.which == 13) {
 		if (input.val()) {
+			d = new Date();
 			socket.emit('message', input.val());
-			$('#conversation').prepend('<div class="animated fadeInRight"><p class="name">' + pseudo + '</p><p class="message">' + escape(input.val()) + '</p><hr></div>');
+			$('#conversation').prepend('<div class="animated fadeInRight"><p class="name">' + "You" + '</p><p class="message my_chats">' + escape(input.val()) + '<div class="timeStamp"> <em>'+(d.getHours()>12?d.getHours()-12:d.getHours())+":"+(d.getMinutes()<10?'0':'')+d.getMinutes()+" "+(d.getHours()>12?"PM":"AM")+'</em></div></p><hr></div>');
 			//Add animation
 			$('#conversation').addClass('animated fadeInDown');
 			//and then remove it when animation ends
