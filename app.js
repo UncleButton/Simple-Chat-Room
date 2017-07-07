30af5b8cc2ce93a6dfcb0950065826f4d9077838
Britton Bailer <britton.bailer@gmail.com>
2017-07-07 18:48:10 -0400

    fixing things as egg

--- app.js
+++ app.js
@@ -18,44 +18,49 @@ app.use(compression());
 
 app.use(express.static(__dirname + '/public'))
 
-	.get('/', function(req, res) {
-		posts.find(function(error, results) {
-			res.render('index.ejs', {posts: results});
-		});
-	});
+.get('/', function(req, res) {
+    posts.find(function(error, results) {
+        res.render('index.ejs', { posts: results });
+    });
+});
 
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
 
-	socket.on('new-user', function(pseudo) {
-		socket.pseudo = pseudo;
-		socket.broadcast.emit('new-user', ent.encode(pseudo));
-		users.push({name: pseudo, id: socket.id});
-		io.emit('update-users', users);
-	});
-	socket.on('disconnect', function() {
-		//socket.pseudo = pseudo;
-		var user = null;
-		users=users.filter(function(u){ console.log(u);
-			if(u.id===socket.id){
-				user=u;
-			}
-			return u.id !== socket.id;
-		});
-		console.log(user);
-		io.emit('user-left', user.name);
-		socket.broadcast.emit('update-users', users);
-	});
+    socket.on('new-user', function(pseudo) {
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('new-user', ent.encode(pseudo));
+        users.push({ name: pseudo, id: socket.id });
+        io.emit('update-users', users);
+    });
 
-	socket.on('message', function(message) {
-		var content = {
-					pseudo: ent.encode(socket.pseudo),
-					message: ent.encode(message)
-				};
-		socket.broadcast.emit('message', content);
-		posts.save(content);
-	});
+    socket.on('disconnect', function() {
+        //socket.pseudo = pseudo;
+        var user = null;
+        users = users.filter(function(u) {
+            console.log(u);
+            if (u.id === socket.id) {
+                user = u;
+            }
+            return u.id !== socket.id;
+        });
+
+        if(u) {
+        	io.emit('user-left', user.name);
+        }
+        
+        socket.broadcast.emit('update-users', users);
+    });
+
+    socket.on('message', function(message) {
+        var content = {
+            pseudo: ent.encode(socket.pseudo),
+            message: ent.encode(message)
+        };
+        socket.broadcast.emit('message', content);
+        posts.save(content);
+    });
 });
 
 console.log("Magic happens at http://localhost:" + PORT);
